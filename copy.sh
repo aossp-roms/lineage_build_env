a2372860cee8b67ce34ae42024b203a213e5dc2d
Pratik Raj <Rajpratik71@gmail.com>
2020-04-09 22:27:26 +0530

    adapting build environment according to base aossp

--- copy.sh
+++ copy.sh
@@ -2,13 +2,13 @@
 
 sudo apt install sshpass
 
-cd /home/ubuntu/android/lineageos/out/target/product/$device/
+cd /home/$USER/android/system/out/target/product/$device/
 
 comp_value=$(echo $lineage_version | cut -d '-' -f 2)
 
 for i in *.zip; do
 	if [[ $i =~ $comp_value ]]; then
 		# Make sure you have access to the remote server via SSH key or password (-p 'password')
-		sshpass scp -o StrictHostKeyChecking=no -r /home/ubuntu/android/lineageos/out/target/product/$device/$i $remote_user@$remote_host:$remote_directory
+		sshpass scp -o StrictHostKeyChecking=no -r /home/$USER/android/system/out/target/product/$device/$i $remote_user@$remote_host:$remote_directory
 	fi
 done
